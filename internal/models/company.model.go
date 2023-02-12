58acefaa0c947b4e4563c1edfb08fa95487bfd92
Eggy fhr <eggifhr@gmail.com>
2023-02-13 01:55:17 +0700

    fix model testing

--- internal/models/company.model.go
+++ internal/models/company.model.go
@@ -1,8 +1,9 @@
 package models
 
 type Company struct {
-	BaseModel
+	ID          string `gorm:"type:char(36)" json:"id"`
+	CreatedAt   string `gorm:"type:string" json:"createdAt" autoUpdateTime:"milli"`
+	UpdatedAt   string `gorm:"type:string" json:"updatedAt" autoUpdateTime:"milli"`
 	Name        string `gorm:"type:string" json:"name" validate:"required,string"`
 	Description string `gorm:"type:string" json:"description" validate:"required,string"`
-	User        []User `gorm:"foreignKey:Company;references:id"`
 }
