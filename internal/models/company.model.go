4908784b1974061f1aa1fd5bdf16f810d09ffdf5
Eggy fhr <eggifhr@gmail.com>
2023-02-14 06:52:33 +0700

    Fix model new

--- internal/models/company.model.go
+++ internal/models/company.model.go
@@ -2,6 +2,7 @@ package models
 
 type Company struct {
 	BaseModel
+	ID          string `gorm:"type:string;primaryKey"`
 	Name        string `gorm:"type:string" json:"name" validate:"required,string"`
 	Description string `gorm:"type:string" json:"description" validate:"required,string"`
 	// User        []User `gorm:"foreignKey:Company;references:id"`
